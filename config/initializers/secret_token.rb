241630ff096bcaa022e37d20100cfac9d89e6eda
Tiago Paulino <tspaulino@gmail.com>
2010-12-21 17:01:41 -0200

    First commit. Real Estate App started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RealEstateApp::Application.config.secret_token = 'dde0e10b7c4ee2fcda72f4f63b7b414858b42cd7909577955a7693e996e174a1daec642f0a82432da7cf32162faf72e9b606f24b33bdeff3e7e13dd1c0dd9852'
